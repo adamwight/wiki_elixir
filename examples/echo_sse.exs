ee2da3877321f36226ee5948edf4bb14739e7f79
Adam Wight <adam.wight@wikimedia.de>
2020-05-12 02:52:18 +0200

    minor formatting

--- examples/echo_sse.exs
+++ examples/echo_sse.exs
@@ -3,34 +3,38 @@
 defmodule DebugMessage do
   def echo_event(message) do
     message
-    |> decode_message_data
-    |> summarize_event
-    |> IO.puts
+      |> decode_message_data
+      |> event_line
+      |> IO.puts
   end
 
   defp decode_message_data(message) do
     message.data
-    |> Poison.decode!
+      |> Poison.decode!
+  end
+
+  defp event_line(data) do
+    data["meta"]["dt"] <> ": " <> summarize_event(data)
   end
 
   defp summarize_event(%{"type" => "categorize"} = data) do
-    ~s(#{data["meta"]["dt"]}: #{data["wiki"]} #{data["title"]} #{data["comment"]} as #{data["title"]} by #{data["user"]})
+    ~s(#{data["wiki"]} #{data["title"]} #{data["comment"]} as #{data["title"]} by #{data["user"]})
   end
 
   defp summarize_event(%{"type" => "edit"} = data) do
-    ~s(#{data["meta"]["dt"]}: #{data["wiki"]} #{data["title"]} edited by #{data["user"]})
+    ~s(#{data["wiki"]} #{data["title"]} edited by #{data["user"]})
   end
 
   defp summarize_event(%{"type" => "log"} = data) do
-    ~s(#{data["meta"]["dt"]}: #{data["wiki"]} #{data["title"]} #{data["log_action"]} by #{data["user"]})
+    ~s(#{data["wiki"]} #{data["title"]} #{data["log_action"]} by #{data["user"]})
   end
 
   defp summarize_event(%{"type" => "new"} = data) do
-    ~s(#{data["meta"]["dt"]}: #{data["wiki"]} #{data["title"]} created by #{data["user"]})
+    ~s(#{data["wiki"]} #{data["title"]} created by #{data["user"]})
   end
 
   defp summarize_event(data) do
-    ~s(#{data["meta"]["dt"]}: #{data["type"]} event: #{Poison.encode!(data)})
+    data["type"] <> " event: " <> Poison.encode!(data)
   end
 end
 
