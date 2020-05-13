7a1cdc979d4a94892006a1ad9281a67c8873dd0d
Adam Wight <adam.wight@wikimedia.de>
2020-05-14 00:01:43 +0200

    Stream continuation

--- examples/echo_action.exs
+++ examples/echo_action.exs
@@ -1,6 +1,10 @@
-WikiAction.get(%{
+WikiAction.stream(%{
   action: :query,
+  format: :json,
   list: :recentchanges,
   rclimit: 2,
 })
-  |> IO.inspect
+|> Stream.take(5)
+|> Enum.flat_map(fn response -> response["query"]["recentchanges"] end)
+|> Enum.map(fn rc -> rc["timestamp"] <> " " <> rc["title"] end)
+|> IO.inspect
--- lib/wiki_action.ex
+++ lib/wiki_action.ex
@@ -1,15 +1,33 @@
 defmodule WikiAction do
   # TODO: These should come from the environment, etc.
+  # TODO: discover API endpoint from wiki domain.
+  # FIXME: underride format: :json, etc. as defaults
   @username Application.get_env(:wiki_elixir, :username)
   @password Application.get_env(:wiki_elixir, :password)
   @endpoint Application.get_env(:wiki_elixir, :action_api)
 
-  # TODO: discover API endpoint from wiki domain.
+  @spec stream(map()) :: Enumerable.t()
+  def stream(params) do
+    Stream.resource(
+      fn -> get(params) end,
+      fn prev ->
+        case prev do
+          %{"continue" => continue} ->
+            next = get(
+              Map.merge(params, continue)
+            )
+            {[next], next}
+
+          _ ->
+            {:halt, nil}
+        end
+      end,
+      fn _ -> end
+    )
+  end
+
   @spec get(map()) :: map()
   def get(params) do
-    # FIXME: underride as defaults
-    params = Map.put(params, :format, :json)
-
     # FIXME: support a base URL with prepended parameters, see HTTPoison.Base.build_query_params
     url = @endpoint <> "?" <> URI.encode_query(params)
     {:ok, response} = Mojito.get(url)
